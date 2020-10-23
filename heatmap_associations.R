371aaed671f9006fa0b63661c7e1fa0173fe96d6
Joe <joerothwell1@gmail.com>
2020-10-23 19:14:28 +0200

    Added extra analyses

--- heatmap_associations.R
+++ heatmap_associations.R
@@ -35,6 +35,7 @@ ggplot(t2a, aes(x = fct_inorder(subsite), y = fct_rev(fct_inorder(exposure)),
 
 # Strength of associations
 t2b <- t2 %>% filter(subsite != "subsite") %>%
+  #filter(analysis == "Diabetes incl") %>%
   mutate(Assoc = ifelse(ci.low > 1, estimate, ifelse(ci.high < 1, -(1/estimate), NA)))
 t2b$exposure <- expnames
 
@@ -55,4 +56,32 @@ ggplot(t2b, aes(x = fct_inorder(subsite), y = fct_rev(fct_inorder(exposure)),
   #xlab("GI cancer") + ylab("Metabolic disorder") +
   facet_grid(fct_inorder(category) ~ ., scales = "free", space = "free") +
   #scale_fill_manual(values = c("#6BAED6", "white", "#FB6A4A")) #+
-  scale_fill_gradient2(na.value = "white")
+  scale_fill_gradient2(na.value = "white") 
+
+
+# 4 analyses as facets
+t2c <- t2 %>% filter(subsite != "subsite") %>%
+  mutate(Assoc = ifelse(ci.low > 1, estimate, ifelse(ci.high < 1, -(1/estimate), NA)))
+t2c$exposure <- rep(expnames, 4)
+
+# Function to transform HRs to heatmap scale
+#heatmap.hr <- function(x) ifelse(x > 1, x, ifelse(x < 1, -(1 / x), NA))
+
+
+ggplot(t2c, aes(x = fct_inorder(subsite), y = fct_rev(fct_inorder(exposure)), 
+                fill = Assoc)) + 
+  geom_tile(colour = "grey", size = 0.5) + theme_minimal() +
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_y_discrete(expand = c(0, 0)) +
+  #scale_fill_gradient2(low="red", high="blue", midpoint = 1) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),
+        strip.text.y.right = element_text(angle = 0, hjust = 0),
+        panel.spacing = unit(1, "lines"),
+        axis.title = element_blank()) +
+  #xlab("GI cancer") + ylab("Metabolic disorder") +
+  facet_grid(fct_inorder(category) ~ analysis, scales = "free", space = "free") +
+  #scale_fill_gradient() #+
+  scale_fill_gradient2(na.value = "white", low = "blue", high = "red") 
+
+
+
