c71bae69fe674798d16e87f32ea13c8513f41918
JoeRothwell <joerothwell1@gmail.com>
2020-10-27 20:07:42 +0100

    Updated

--- appendectomy_copy.R
+++ appendectomy_copy.R
@@ -9,47 +9,22 @@ t2 <- read_xlsx("CRC-appendectomy meta-analysis.xlsx") %>%
   arrange(fct_inorder(subsite), fct_inorder(group), fct_inorder(cohort)) %>% 
   mutate(std.err = ((ci.upper-ci.lower)/2)/qnorm(0.975))
 
-# Layout sheet used to calculate spacings
+# Layout sheet used to calculate spacings, row positions and subset df
 ly <- read_xlsx("digestive_cancers.xlsx", sheet = 3)
-
+rowvec <- c(-0.5,4.5,10.5, 17.5,22.5,28.5, 35.5,40.5,46.5, 53.5,58.5,64.5, 71.5,76.5,82.5)
 df <- data.frame(t2[, c(1,9)])
 
-# Perform meta-analyses (to replace hard codings with an lapply expression)
+# Perform meta-analyses on groups with nest
 library(metafor)
-ma1 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 1:3)
-ma2 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 4:6)
-ma3 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 7:8)
-ma4 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 9:11)
-ma5 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 12:14)
-ma6 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 15:16)
-ma7 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 17:19)
-ma8 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 20:22)
-ma9 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 23:24)
-ma10 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 25:27)
-ma11 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 28:30)
-ma12 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 31:32)
-ma13 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 33:35)
-ma14 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 36:38)
-ma15 <- rma(hr, sei = std.err, data=t2, method="REML", subset = 39:40)
-
-ll <- list(ma15,ma14,ma13,ma12,ma11,ma10,ma9,ma8,ma7,ma6,ma5,ma4,ma3,ma2,ma1)
-rowvec <- c(-0.5,4.5,10.5, 17.5,22.5,28.5, 35.5,40.5,46.5, 53.5,58.5,64.5, 71.5,76.5,82.5)
-
-# Extract data for table
-results <- lapply(rev(ll), "[", c("b", "ci.lb", "ci.ub", "I2", "QEp"))
-append.yn <- do.call(rbind, results) %>% as_tibble
-
-# Or using nest
 mas <- t2 %>% group_by(subsite, group) %>% nest() %>% 
   mutate(mods = lapply(data, function(df) rma(hr, sei = std.err, data=df, method="REML")))
 ll1 <- mas$mods
 
 # Extract data for table
-results1 <- lapply(ll1, "[", c("b", "ci.lb", "ci.ub", "I2", "QEp"))
+results1 <- lapply(mas$mods, "[", c("b", "ci.lb", "ci.ub", "I2", "QEp"))
 append.yn1 <- do.call(rbind, results1) %>% as_tibble
 
 # Complex plot with spacings
-library(metafor)
 par(mar=c(5,4,1,2), mgp = c(2,0.5,0))
 li <- c(-2.1, -2.8)
 forest(t2$hr, ci.lb = t2$ci.lower, ci.ub = t2$ci.upper, xlab = "Hazard ratio", pch = 18, 
@@ -60,16 +35,16 @@ forest(t2$hr, ci.lb = t2$ci.lower, ci.ub = t2$ci.upper, xlab = "Hazard ratio", p
        ilab.pos = 4, ilab.xpos = li, refline = 1)
 
 # Add meta-analyses and text in a loop
-for(ind in 1:15) addpoly(ll[[ind]], rowvec[ind], efac = 0.8, mlab = NA, cex = 0.7, col = "grey")
+for(ind in 1:15) addpoly(ll1[[ind]], rev(rowvec)[ind], efac = 0.8, mlab = NA, cex = 0.7, col = "grey")
 text(li, 89, c("Cohort", "Group"), pos = 4, cex = 0.7)
 
 # Positions of p-het and I2 are 22 and 25
-I2s <- round(unlist(sapply(ll, "[", 25)), 1)
-phets <- round(unlist(sapply(ll, "[", 22)), 2)
+I2s <- round(unlist(sapply(ll1, "[", 25)), 1)
+phets <- round(unlist(sapply(ll1, "[", 22)), 2)
 plabs <- function(x, y) as.expression(bquote("RE model"~I^2 * "=" ~ .(x)* "%" * ", p ="~ .(y) ))
 
 # Function name comes first in mapply (opposite to sapply)
-text(li[1], rowvec, labels = mapply(plabs, I2s, phets), cex = 0.7, pos = 4)
+text(li[1], rev(rowvec), labels = mapply(plabs, I2s, phets), cex = 0.7, pos = 4)
 
 
 # Basic plot, no spacing (not used)
