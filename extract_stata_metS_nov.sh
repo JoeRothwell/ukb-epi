416e04e77064210b3b475c906fd83e4fb6eaabdd
Joe <joerothwell1@gmail.com>
2021-04-11 23:08:27 +0200

    Added new PRS models

--- extract_stata_metS_nov.sh
+++ extract_stata_metS_nov.sh
@@ -20,6 +20,8 @@ egrep "ent:|diabet?_1|arm_1|CEP_1|IDF2_1" > crc_earlyonset.txt
 
 # Polygenic risk scores: associations with CRC and pancreatic cancer
 egrep "_scale" LOG_assoc_PRS.txt > PRS_assoc.txt
+# Adjusted genotyping array and genetic ancestry PCs
+egrep "_scale" LOG_PRS_adj.txt > PRS_assoc_adj.txt
  
 # PRS stratified models for metS/diabetes
 egrep "ent:|diabet?_1|arm_1|CEP_1|IDF2_1|_scale" LOG_metS_PRS.txt > metS_PRS.txt
@@ -30,3 +32,10 @@ egrep "ent:|diabet?_1" LOG_diab_PRS.txt > diab_PRS.txt
 egrep "ent:|diabet?_1|arm_1|CEP_1|IDF2_1|_scale" LOG_metS_PRS_bm.txt > metS_PRS_bm.txt
 egrep "ent:|diabet?_1" LOG_prediab_PRS_bm.txt > prediab_PRS_bm.txt
 egrep "ent:|diabet?_1" LOG_diab_PRS_bm.txt > diab_PRS_bm.txt
+
+# PRS adjusting for genotyping array and 15 genetic ancestry PCs
+egrep "ent:|diabet?_1|arm_1|CEP_1|IDF2_1|_scale" LOG_metS_PRS_adj.txt > metS_PRS_adj.txt
+egrep "failures|ent:|_It" LOG_metS_PRS_adj.txt
+
+egrep "ent:|diabet?_1|arm_1|CEP_1|IDF2_1|_scale" LOG_metS_PRS_adj.txt > metS_PRS_adj1.txt
+egrep "failures|ent:|_It" LOG_metS_PRS_adj.txt
