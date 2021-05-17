98200b9b31c5b4c1d9eedcf5b6fc7b12cd7f76ac
Joe <joerothwell1@gmail.com>
2021-05-17 10:15:34 +0200

    Renamed file

--- Baseline_tables_UKB.R
+++ Baseline_tables_UKB.R
@@ -7,13 +7,13 @@ options(qwraps2_markup = "markdown")
 
 
 # For metS paper
-dat <- read_dta("ukb_metS_table4.dta")
+dat <- read_dta("ukb_metS_table_med.dta")
 dat <- dat %>% select(-id) %>% 
   mutate_at(vars(digestive_inc, ever_horm, alc_stat, smoke_stat, sex, q4_depr, fh_crc, metS_harm, 
-                 med_aspib, PRS_crc_cat3, PRS_pan_cat3), as.factor) %>%
+                 med_aspib), as.factor) %>%
   select(digestive_inc, age_assess, sex, bmi_m, waist_c, height_m, pa_total_mets, smoke_stat, alc_stat, q4_depr, dpr_index,
          fh_crc, med_aspib, redprocmeat, bp_systolic, bp_diastolic, hba1c, hdl, tryg, metS_harm,
-         PRS_crc_cat3, PRS_pan_cat3)
+         PRS_crc, PRS_pan)
 
 # Generate table automatically
 sumtab <- qsummary(dat, numeric_summaries = list("Mean (SD)" = "~ mean_sd(%s)"),
