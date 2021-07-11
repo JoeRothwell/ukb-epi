51c355e4f30a9d7e7b4fbdf9cb3bdaff1d02c7a9
Joe <joerothwell1@gmail.com>
2021-07-11 14:13:58 +0200

    Rename

--- ICCs_UKB.R
+++ ICCs_UKB.R
@@ -5,3 +5,10 @@ UKBss <- UKB_final %>% select(f.eid, f.21003.0.0, f.21003.1.0, f.21003.1.0, f.30
                               f.21001.0.0, f.21001.1.0, f.21001.2.0)
 
 saveRDS(UKBss, file = "UKB_for_ICC.rds")
+
+# Translation of ICC_metS.do into R
+readRDS(ukb, "UKB_for_ICC.rds")
+ukb %>% rename(id = f.eid,
+               age_assess1 = f.21003.0.0, age_assess2 = f.21003.1.0, age_assess3 = f.21003.2.0,
+               bmi_1 = f.21001.0.0, bmi_2 = f.21001.1.0, bmi_3 = f.21001.2.0,
+               igf_1 = f.30770.0.0, igf_2 = f.30770.1.0, sex = f.1)
