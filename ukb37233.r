2ef67b78871a20817be71d70f11a3505543efe09
JoeRothwell <joerothwell1@gmail.com>
2021-09-25 08:31:26 +0200

    Removed files from version control

--- ukb37233.r
+++
@@ -1,7 +0,0 @@
-# R program ukb37233.tab created 2019-11-07 by ukb2r.cpp Mar 14 2018 16:01:22
-library(tidyverse)
-#bd <- read.table("C:\\Users\\Rothwell\\Downloads\\UKB\\ukb_R\\ukb37233.tab", header=TRUE, sep="\t")
-bd <- read_tsv("C:\\Users\\Rothwell\\Downloads\\UKB\\ukb_R\\ukb37233.tab")
-
-ukb37233 <- bd
-rm(bd)
