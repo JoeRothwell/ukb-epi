54f260f1f5eff599b69b30c41a7ba3522aa449b6
Joe <joerothwell1@gmail.com>
2022-01-16 15:39:55 +0100

    Updated

--- extract_stata_metabo.sh
+++ extract_stata_metabo.sh
@@ -1,22 +1,22 @@
-# Extract data from Stata log files, reorganised November 2020
-# Analyses are now grouped by inclusions (metS, pre-diabetes, diabetes)
+# Extract data from Stata log files. Amino acids analysis and other metabolite analyses
 cd /Users/joe/Documents/Stata
 
-# Amino acids, all CRC subsites
-egrep "ent:|ala|gl|his|leu| iso| val|phe|tyr" LOG_crc_amino_fast2021.txt > CRC_amino2021.txt
+# Amino acids, all CRC subsites (last updated after correction of 2019 results)
+egrep "ent:|ala|gl|his|leu| iso| val|phe|tyr" LOG_crc_amino_fast2021a.txt > CRC_amino2021.txt
 
 # Categorical ref Q1
-egrep "ent:|ala|Igl|his|leu|iso|val|phe|tyr" LOG_CRC_amino_cat2021.txt > CRC_amino_cat.txt
-egrep "ent:|ala|Igl|his|iso|leu|val|phe|tyr" LOG_crc_amino_cat_fast2021.txt > CRC_amino_cat_fast.txt
+#egrep "ent:|ala|Igl|his|leu|iso|val|phe|tyr" LOG_CRC_amino_cat2021.txt > CRC_amino_cat.txt
+egrep "ent:|ala|Igl|his|iso|leu|val|phe|tyr" LOG_crc_amino_cat_fast2021a.txt
 
 # Trend test (same model but without i. in Stata)
-egrep "ent:|ala_|gln_|gly_|his_|iso_|leu_|val_|phe_|tyr_" LOG_crc_amino_trend2021.txt
+egrep "ent:|ala_|gln_|gly_|his_|iso_|leu_|val_|phe_|tyr_" LOG_crc_amino_trend2021a.txt
 
-# Continuous fasting
+# Exclude early diagnoses (no exclusions and 2-year lag)
+egrep "ent:|ala|gl|his|leu|iso| val|phe|tyr" LOG_amino_exclude_early2021a.txt
+egrep "failures" LOG_amino_exclude_early2021a.txt
+
+# Continuous fasting only (not used)
 egrep "ent:|ala|gl|his|leu| val|phe|tyr" LOG_CRC_amino_fast.txt > CRC_amino_fast.txt
-
-# Exclude early diagnoses
-egrep "ent:|ala|gl|his|leu| val|phe|tyr" LOG_amino_exclude_earlyCRC.txt > CRC_amino_exclude early.txt
 
 # ICCs
 egrep "id " LOG_ICC_metabo.txt | egrep -v "Number of groups" > ICC_168metab.txt
